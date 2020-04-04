34efcf8b80fcde6456733e53a01b9c91cbc4fb1c
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2020-04-04 16:17:29 +0200

    Another try

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
   $('.gallery-embed-script, .gallery-embed-html').addClass('main-container container-fluid');
 });
 
+$(document).ready(function () {
 $(".utterances-frame").on("load", function() {
   let head = $(".utterances-frame").contents().find("head");
   let css = '<style>.timeline-header {display: none;}</style>';
   $(head).append(css);
+});
 });
